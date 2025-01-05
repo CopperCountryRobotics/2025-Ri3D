a3fc80e3dff505ee0bd2e294aedc6ceea0feb761
DESKTOP-TUVF67S\Will <kanarwm@outlook.com>
2025-01-04 19:11:19 -0500

    Made subsystem

--- src/main/java/frc/robot/subsystems/elevator/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorSubsystem.java
@@ -1,5 +1,55 @@
 package frc.robot.subsystems.elevator;
 
-public class ElevatorSubsystem {
-  
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import com.revrobotics.spark.SparkMax;
+
+import frc.robot.subsystems.drive.DriveConstants;
+import frc.robot.util.SparkUtil;
+
+public class ElevatorSubsystem extends SubsystemBase{
+    private final SparkMax motor;
+    private final RelativeEncoder encoder;
+    public ElevatorSubsystem(){
+        motor = new SparkMax(DriveConstants.leftOneCANID, MotorType.kBrushless);
+        encoder = motor.getEncoder();
+        SparkMaxConfig globalConfig = new SparkMaxConfig();
+        globalConfig.smartCurrentLimit(50).idleMode(IdleMode.kBrake).inverted(false);
+        SparkUtil.tryUntilOk(5, 
+        () -> motor.configure(globalConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters));
+    }
+
+    /**
+     * Set the motion of the elevator. Positive is up.
+     * @param speed
+     */
+    public void run(double speed){
+        motor.set(speed);
+    }
+
+    public boolean atMaxHeight(){
+        if(motor.get() > 0.01 
+        && Math.abs(encoder.getVelocity()) < 0.1 
+        && motor.getOutputCurrent() > 15){
+            return true;
+        }
+        return false;
+    }
+
+    public boolean atMinHeight(){
+        if(motor.get() < 0.01 
+        && Math.abs(encoder.getVelocity()) < 0.1 
+        && motor.getOutputCurrent() > 15){
+            return true;
+        }
+        return false;
+    }
+
 }
