2b61228cdb5535e78cd3481262f409e6f5b79565
DESKTOP-TUVF67S\Will <kanarwm@outlook.com>
2025-01-06 00:08:18 -0500

    Fixed existing errors

--- src/main/java/frc/robot/commands/IntakeCommand.java
+++ src/main/java/frc/robot/commands/IntakeCommand.java
@@ -4,10 +4,10 @@
 
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.subsystems.Intake;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.intake.Intake;
 
-public class IntakeCommand extends CommandBase {
+public class IntakeCommand extends Command {
   private final Intake intake;
   private boolean direction;
   private boolean stop;
